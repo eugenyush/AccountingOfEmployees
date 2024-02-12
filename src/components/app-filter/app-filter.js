f906fdcfde56a6edd7a9cd17fb8b9bc7951a5715
Yauhen <yushkevicheugen@gmail.com>
2024-02-12 17:15:12 +0100

    Add function for filter

--- src/components/app-filter/app-filter.js
+++ src/components/app-filter/app-filter.js
@@ -1,27 +1,30 @@
-import './app-filter.css'
+import "./app-filter.css";
 
-const AppFilter = () =>{
-    return(
+const AppFilter = (props) => {
+    const buttonsData = [
+        {name: 'all', label: 'All employees'},
+        {name: 'rise', label: 'For a raise'},
+        {name: 'salary', label: 'Salary is more than $ 1000'}
+    ];
+
+    const buttons = buttonsData.map(({name, label}) => {
+        const active = props.filter === name;
+        const clazz = active ? 'btn-light' : 'btn-outline-light';
+        return (
+            <button type="button"
+                    className={`btn ${clazz}`}
+                    key={name}
+                    onClick={() => props.onFilterClick(name)}>
+                    {label}
+            </button>
+        )
+    })
+
+    return (
         <div className="btn-group">
-            <button 
-            className="btn btn-light"
-            type="button">
-                All employees
-            </button>
-            <button 
-            className="btn btn-outline-light"
-            type="button">
-                For a raise
-            </button>
-            <button 
-            className="btn btn-outline-light"
-            type="button">
-                Salary is more than $ 1000
-            </button>
-            
+            {buttons}
         </div>
     )
 }
-
 
 export default AppFilter;
